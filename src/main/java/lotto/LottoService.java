629ece93b94fddb05d08da88e80073cf74e00332
Giyoul <cla12093@gmail.com>
2025-10-31 17:29:55 +0900

    feat(domain): update get lotto numbers feature

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -1,2 +1,16 @@
-package lotto;public class LottoService {
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class LottoService {
+
+
+    public void purchaseLottos(){
+
+    }
+
+    List<Integer> getLottoNumbers(){
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
 }
