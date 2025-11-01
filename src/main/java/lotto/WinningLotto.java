c7499051688c342f5759bcb14e8f9b8312f85e22
Giyoul <cla12093@gmail.com>
2025-11-01 17:43:17 +0900

    refactor: update WinningLotto class instance variable
    From:
    List<Integer> lottoNumbers
    
    To:
    Lotto lotto
    Integer bonusNumber

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -3,9 +3,16 @@ package lotto;
 import java.util.List;
 
 public class WinningLotto {
-    private final List<Integer> lottoNumbers;
+    private final Lotto lotto;
+    private final Integer bonusNumber;
 
     public WinningLotto(List<Integer> lottoNumbers) {
-        this.lottoNumbers = lottoNumbers;
+        this.lotto = new Lotto(lottoNumbers);
+        bonusNumber = null;
+    }
+
+    public WinningLotto(List<Integer> lottoNumbers, Integer bonusNumber) {
+        this.lotto = new Lotto(lottoNumbers);
+        this.bonusNumber = bonusNumber;
     }
 }
