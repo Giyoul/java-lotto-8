7ca2791171e3a4c81bd58dc0d827ff659ee74a8e
Giyoul <cla12093@gmail.com>
2025-11-03 00:05:36 +0900

    feat(validation): add bonus number boundary validation feature

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -6,7 +6,14 @@ public class WinningLotto {
 
     public WinningLotto(Lotto lotto, Integer bonusNumber) {
         this.lotto = lotto;
+        validateBoundary(bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBoundary(Integer bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
     }
 
     public int countMatchNumbers(Lotto userLotto) {
