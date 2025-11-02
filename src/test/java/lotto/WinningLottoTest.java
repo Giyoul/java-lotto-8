f05b7bfcbf10bc742cb3da3af34ce86b82f20681
Giyoul <cla12093@gmail.com>
2025-11-03 00:05:10 +0900

    test(validation): add bonus number boundary validation test

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
@@ -17,5 +18,17 @@ public class WinningLottoTest {
 
         // Then
         assertTrue(result, "보너스 번호가 포함되어 있는지 정확하게 확인한다");
+    }
+
+    @Test
+    void 보너스_번호가_1부터_45_사이의_숫자가_아닌_경우_예외가_발생한다() {
+        // Given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Integer input = 46;
+
+        // When & Then
+        assertThrows(IllegalArgumentException.class,
+                () -> new WinningLotto(lotto, input)
+        );
     }
 }
